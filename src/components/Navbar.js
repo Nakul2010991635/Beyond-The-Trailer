d30586ecbbf2336e4f08e118aa574401d46940d1
Nakul <nakul1635.be20@chitkara.edu.in>
2024-04-09 22:58:25 +0530

    added-login-functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import Logo from './PageLogo.js';
-import { MenuRounded, Info } from '@material-ui/icons';
-
+import { Info} from '@material-ui/icons';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HistoryIcon from '@material-ui/icons/History';
+const handleLogout = () => {
+	localStorage.removeItem("token");
+	window.location.reload();
+};
 
 class Navbar extends Component {
+	
 
 	render() {
 		return (
 			<div className='nav-logo'>
 				<div className='nav-link'
+					onClick={this.props.onProfileClick}
+				>
+					<div className='nav-link-icon'>
+						<AccountCircleIcon size='large' style={{ filter: 'url(#shadow)' }} />
+					</div>
+					<span className='nav-link-text'>Profile</span>
+				</div>
+				<div className='nav-link'
 					onClick={this.props.onMenuClick}
 				>
 					<div className='nav-link-icon'>
-						<MenuRounded size='large' style={{ filter: 'url(#shadow)' }} />
+						<HistoryIcon size='large' style={{ filter: 'url(#shadow)' }} />
 					</div>
 					<span className='nav-link-text'>History</span>
 				</div>
@@ -25,6 +40,17 @@ class Navbar extends Component {
 						<Info size='large' />
 					</div>
 					<span className='nav-link-text'>ABOUT</span>
+					
+				</div>
+				<div 
+					className='nav-link'
+					onClick={handleLogout}
+				>
+					<div className='nav-link-icon'>
+						<ExitToAppIcon size='large' />
+					</div>
+					<span className='nav-link-text'>LOGOUT</span>
+					
 				</div>
 			</div>
 		);
